aa0d7661f9c59ae625b60b2854440921b42f7f92
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-07-18 16:56:27 -0700

    initial file structure and some other stuff

---
+++ snacks/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView, DetailView
+from .models import Snack
+
+
+class SnackListView(ListView):
+    template_name = "snack_list.html"
+    model = Snack
+    context_object_name = 'snack'
+
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
